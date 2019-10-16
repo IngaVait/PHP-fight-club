6b8afb86fa898f9eed488160a576bef7a66252bc
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-16 14:37:42 +0300

    po patikrinimo - clean wallet

--- index.php
+++ index.php
@@ -3,28 +3,28 @@ $months = 12;
 $wallet = 1000;
 $month_income = 700;
 
-for ($i = 0; $i < 12; $i++) {
+for ($i = 1; $i < $months; $i++) {
     $wallet += $month_income - rand(500, 1000);
-    if ($wallet <= 0) {
+    if ($wallet < 0) {
         break;
     }
 }
 
-if ($wallet <= 0) {
-    $h2 = "Atsargiai, $i menesi gali baigtis pinigai";
-} else
-    $h2 = "Po $months menesiu prognozuojamas likutis: $wallet";
-
 $h1 = 'Wallet Forecast';
-?>
 
+if ($wallet < 0) {
+    $h2 = "Atsargiai, $i menesi gali baigtis pinigai";
+} else{
+    $h2 = "Po $months menesiu prognozuojamas likutis: $wallet";
+}
+?>
 <!doctype html>
 <html lang="en">
     <head>
         <title>Wallet Forecast</title>
     </head>
     <body>
-        <h1><?php print $h1 ?></h1>
-        <h1><?php print $h2 ?></h1>
+        <h1><?php print $h1; ?></h1>
+        <h1><?php print $h2; ?></h1>
     </body>
 </html>
