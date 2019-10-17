df5ce851c864d4710c0c9fc527f733fdd7d1aa2b
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-17 14:41:49 +0300

    teisinga uzduotis su kuru

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
 $petrol_in_tank = rand(20, 30);
 $diesel_in_tank = 0;
-$diesel_limit = ($petrol_in_tank + $diesel_in_tank) / 10;
 
-for ($i = $diesel_in_tank; $i <= $diesel_limit; $i++) {
-   $tank = $petrol_in_tank + $diesel_in_tank;
-   if($i == $tank / 10) {
-       break;
-   }
- }
+for ($diesel_in_tank = 0; $diesel_in_tank <= ($petrol_in_tank + $diesel_in_tank) / 10; $diesel_in_tank+=0.5) {
+    }
 
 $h1 = 'Pripylei dyzelio į benz. mašiną?';
 $h2 = "Benzino buvo: $petrol_in_tank";
-$h3 = "Max dyzelio riba: $i";
+$h3 = "Max dyzelio riba: $diesel_in_tank";
 ?>
 <!doctype html>
 <html lang="en">
