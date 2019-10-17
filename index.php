7a6624cd57d86623a0137196f933438cc0aa8e92
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-17 13:45:50 +0300

    Dyzelis bake

--- index.php
+++ index.php
@@ -1,42 +1,28 @@
 <?php
-$days = 365;
-//$cigs_mon_fri = rand(3, 5);
-//$cigs_sat = rand(20, 40);
-//$cigs_sun = rand(1, 3);
-$pack_price = 3.5;
-$count_mon_fri = 0;
-$count_sat = 0;
-$count_sun = 0;
-$time_per_cig = 5;
+$petrol_in_tank = rand(20, 30);
+$tank = $petrol_in_tank;
+$diesel_in_tank = 0;
+$diesel_limit = ($petrol_in_tank + $diesel_in_tank) / 10;
 
-for ($i = 1; $i <= $days; $i++) {
-    $strtotime_param = "+$i days";
-    $number = date("N", strtotime($strtotime_param));
-    if ($number <= 5) {
-        $count_mon_fri += rand(3, 5);
-    } elseif ($number == 6) {
-        $count_sat += rand(20, 40);
-    } else {
-        $count_sun += rand(1, 3);
+for ($i = $diesel_in_tank; $i <= $diesel_limit; $i++) {
+    $diesel_in_tank += 0.5;
+    $tank += $petrol_in_tank;
+    if ($diesel_in_tank == $tank / 10) {
+        break;
     }
 }
-$h1 = 'Tavo rūkymo statistika';
-$count = $count_mon_fri + $count_sat + $count_sun;
-$money = ceil($count / 20) * $pack_price;
-$count_min = $count * $time_per_cig;
-$count_time = intdiv($count_min, 60).':'. ($count_min % 60);
-$h2 = "Jei ir toliau rūkysi taip pat, per ateinančias $days dienas būsi prastovėjęs lauke $count_time valandų. <br> Ir ne tik surūkęs $count vnt. cigarečių, bet ir dūmais paleidęs $money eur.";
+$h1 = 'Pripylei dyzelio į benz. mašiną?';
+$h2 = "Benzino buvo: $petrol_in_tank";
+$h3 = "Max dyzelio riba: $diesel_limit";
 ?>
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Kiek tu surukai</title>
+        <title>Dyzelio bake problema</title>
     </head>
     <body>
         <h1><?php print $h1; ?></h1>
         <h1><?php print $h2; ?></h1>
-        <?php for ($img = 1; $img <= $count; $img++) : ?>
-            <img src="<?php print "../images/Cig.PNG"; ?>" alt="Cig"/>
-        <?php endfor ?>
+        <h1><?php print $h3; ?></h1>
     </body>
 </html>
