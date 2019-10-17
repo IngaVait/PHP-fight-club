4bb250555da2d2d84578ea93e4dfc1276e4c6d8b
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-17 14:19:48 +0300

    kuras bake

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
 $petrol_in_tank = rand(20, 30);
-$tank = $petrol_in_tank;
 $diesel_in_tank = 0;
 $diesel_limit = ($petrol_in_tank + $diesel_in_tank) / 10;
 
 for ($i = $diesel_in_tank; $i <= $diesel_limit; $i++) {
-    $diesel_in_tank += 0.5;
-    $tank += $petrol_in_tank;
-    if ($diesel_in_tank == $tank / 10) {
-        break;
-    }
-}
+   $tank = $petrol_in_tank + $diesel_in_tank;
+   if($i == $tank / 10) {
+       break;
+   }
+ }
+
 $h1 = 'Pripylei dyzelio į benz. mašiną?';
 $h2 = "Benzino buvo: $petrol_in_tank";
-$h3 = "Max dyzelio riba: $diesel_limit";
+$h3 = "Max dyzelio riba: $i";
 ?>
 <!doctype html>
 <html lang="en">
