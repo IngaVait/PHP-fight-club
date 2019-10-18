57564942dff398ad361760bd4600328651af31ab
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-18 10:33:01 +0300

    kada isigys + kupiuros parodytos

--- index.php
+++ index.php
@@ -10,69 +10,95 @@ for ($i = $car_price_new; $i >= $santaupos; $i *= 0.98) {
 }
 $car_price_used = round($car_price_used, 2);
 $depr_perc = 100 - round($car_price_used * 100 / $car_price_new);
-$h1 = 'Kiek nuvertes masina';
-$h2 = "Naujos masinos kaina $car_price_new eur.";
-$h3 = "Sia masina galesi nusipirkti po $months men., kai jos verte bus $car_price_used";
-$h4 = "Masina nuvertes $depr_perc proc.";
-
-for ($count_money = $car_price_used; $count_money > 500; $count_money -= 500) {
-    $img_500eur = "img_500eur";
-}
-
-for ($to_200 = $count_money; $to_200 > 200; $to_200 -= 200) {
-    $img_200eur = "img_200eur";
-}
-
-for ($to_100 = $to_200; $to_100 > 100; $to_100 -= 100) {
-    $img_100eur = "img_100eur";
-}
-
-for ($to_5 = $to_100; $to_5 > 5; $to_5 -= 5) {
-    $img_5eur = "img_5eur";
-}
-
-for ($to_20ct = $to_5; $to_20ct > 0.2; $to_20ct -= 0.2) {
-    $img_20ct = "img_20ct";
-}
-
-for ($to_02ct = $to_20ct; $to_02ct > 0.02; $to_02ct -= 0.02) {
-    $img_02ct = "img_02ct";
-}
-
-for ($to_01ct = $to_02ct; $to_01ct > 0.01; $to_01ct -= 0.01) {
-    $img_01ct = "img_01ct";
-}
-var_dump($to_01ct);
-
-//
-//$img_500eur = 'http://www.ekspertai.eu/static/uploads/cache/800x600/2016-05-02/61347-500euros-jpeg.jpg';
-//$img_50eur = 'https://www.bankai.lt/upload/banknotai/big/2014/03/euro-2002-50-euros-f-17714.jpg';
-//$img_20eur = 'https://www.bankai.lt/upload/banknotai/big/2015/07/euro-2002-20-euros-f-33253.jpg';
-//$img_10eur = 'https://www.bankai.lt/upload/banknotai/big/2014/03/euro-2014-10-euro-f-17700.jpg';
-//$img_100eur = 'http://www.santarve.lt/images/articles/2012/08/100-3.gif';
-//$img_200eur = 'https://www.bankai.lt/upload/banknotai/big/2014/03/euro-2002-200-euros-f-17706.jpg';
-//$img_5eur = 'https://www.seb.lt/sites/default/files/web/images/5euro_back_HR_europa.jpg';
-//$img_2eur = 'https://i.colnect.net/f/3863/512/2-Euro.jpg';
-//$img_1eur = 'http://www.kalykla.lt/image/data/naujienos/1Eur_rev.png';
-//$img_50ct = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQEM7Mg4VtN95HbSaT_-2smB_09_pK_jm2gUWkXwV_iZrbFE9qd';
+$h1 = 'Kiek nuvertės tavo svajonių mašina';
+$h2 = "Naujos mašinos kaina $car_price_new eur.";
+$h3 = "Tu ją galėsi nusipirkti po $months men., kai jos vertė bus $car_price_used";
+$h4 = "Mašina jau bus nuvertėjusi $depr_perc proc.";
+$img_500eur = 'img_500eur';
+$img_200eur = 'img_200eur';
+$img_5eur = 'img_5eur';
+$img_20ct = 'img_20ct';
+$img_02ct = 'img_02ct';
 ?>
 <!doctype html>
 <html lang="en">
     <head>
         <title>Masinos reikalai</title>
         <style>
-
+            header {
+                background: url(https://www.euroexchangeratenews.co.uk/wp-content/uploads/2016/06/Euro-note-denominations.jpg);
+            }
+            section {
+                background: rgba(128, 0, 42, .7);
+                color: lightgray;
+                display: flex;  
+                flex-direction: column;
+                align-items: center;               
+            }
+            main {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-around;
+                align-items: center;
+            }
+            div {
+                width: 229px;
+                height: 118px;
+            }
+            .img_500eur {
+                background: url(http://www.ekspertai.eu/static/uploads/cache/800x600/2016-05-02/61347-500euros-jpeg.jpg);
+                background-size: cover;
+            }
+            .img_200eur {
+                background: url(https://www.bankai.lt/upload/banknotai/big/2014/03/euro-2002-200-euros-f-17706.jpg);
+                background-size: cover;
+            }
+            .img_5eur {
+                background: url(https://www.seb.lt/sites/default/files/web/images/5euro_back_HR_europa.jpg);
+                background-size: cover;
+            }  
+            .img_20ct {
+                background: url(https://i.colnect.net/f/3362/572/20-Euro-Cent-Bratislava-Castle.jpg);
+                background-repeat: no-repeat;
+                background-size: 70px;
+                width: 80px;
+                padding: 5px;
+            }  
+            .img_02ct {
+                background: url(https://i.colnect.net/f/2719/000/2-Euro-Cent.jpg);
+                background-repeat: no-repeat;
+                background-size: 70px;     
+                width: 80px;
+                padding: 5px;
+            }
         </style>
     </head>
     <body>
-        <h1><?php print $h1; ?></h1>
-        <h2><?php print $h2; ?></h2>
-        <h3><?php print $h3; ?></h3>
-        <h4><?php print $h4; ?></h4>
-
-        
-
-        <!--    namu darbe img daryti kaip divo bagrounda, ir koks tas bg apspres jo klase, tai greiciausiai cikla reikes kazkaip idomiai per tai prasukti/-->
-
+        <header>
+            <section>
+                <h1><?php print $h1; ?></h1>
+                <h2><?php print $h2; ?></h2>
+                <h3><?php print $h3; ?></h3>
+                <h4><?php print $h4; ?></h4>
+            </section>
+        </header>
+        <hr>
+        <main>
+            <?php for ($count_money = $car_price_used; $count_money > 500; $count_money -= 500) : ?>
+                <div class="<?php print $img_500eur; ?>" > </div>
+            <?php endfor; ?>
+            <?php for ($to_200 = $count_money; $to_200 > 200; $to_200 -= 200) : ?>
+                <div class="<?php print $img_200eur; ?>" > </div>
+            <?php endfor; ?>
+            <?php for ($to_5 = $to_200; $to_5 > 5; $to_5 -= 5) : ?>
+                <div class="<?php print $img_5eur; ?>" > </div>
+            <?php endfor; ?>
+            <?php for ($to_20ct = $to_5; $to_20ct > 0.2; $to_20ct -= 0.2) : ?>
+                <div class="<?php print $img_20ct; ?>" > </div>
+            <?php endfor; ?>
+            <?php for ($to_02ct = $to_20ct; $to_02ct > 0.02; $to_02ct -= 0.018) : ?>
+                <div class="<?php print $img_02ct; ?>" > </div>
+            <?php endfor; ?>
+        </main>
     </body>
 </html>
