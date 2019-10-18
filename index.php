e92e957a74ca74ddcb6af268bf166dc1e5f9a889
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-18 15:42:39 +0300

    array - bigger

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-$array = ['kiausinis', 'baklazanas', 'obuolys'];
+$array = [
+    [
+        "name" => 'kiausiniai',
+        "quant" => 2,
+        "size" => 'big',
+    ],
+    [
+        "name" => 'baklazanas',
+        "quant" => 1,
+        "size" => 'small',
+    ],
+    [
+        "name" => 'grietine',
+        "quant" => 1,
+        "size" => 'big',
+    ],
+];
+    
 var_dump($array);
