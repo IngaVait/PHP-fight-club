dd3b9242cbdd2cd02e4f481bbc72edceb58b5759
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-21 15:43:48 +0300

    race track

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
 <?php
 
-$array = [
-    'lane_1st' => [
-        'pins_standing' => [
-            'row_1' => [1],
-            'row_2' => [0, 1],
-            'row_3' => [0, 0, 1],
-            'row_4' => [1, 0, 0, 1],
-        ],
-        'in_condition' => true,
+$race_track = [
+    'corner_1' => [
+        'right_side' => true,
+        'angle' => 5,
+        'straight_after' => 500,
     ],
-    'lane_2st' => [
-        'pins_standing' => [
-            'row_1' => [0],
-            'row_2' => [0, 0],
-            'row_3' => [0, 0, 0],
-            'row_4' => [0, 0, 0, 0],
-        ],
-        'in_condition' => false,
+      'corner_0' => [
+        'right_side' => false,
+        'angle' => 2.5,
+        'straight_after' => 5,
     ],
-];
-var_dump($array);
+      'corner_3' => [
+        'right_side' => true,
+        'angle' => 5,
+        'straight_after' => 1000,
+    ],
+      'corner_4' => [
+        'right_side' => false,
+        'angle' => 4,
+        'straight_after' => 10,
+    ],
+      'corner_5' => [
+        'right_side' => true,
+        'angle' => 10,
+        'straight_after' => 200,
+    ],
+    'corner_6' => [
+        'right_side' => true,
+        'angle' => 3,
+        'straight_after' => 900,
+    ],
+ ];
+ var_dump($race_track);
