3a2ab84603b2a08b64dbc9940a62e740d3c41201
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-21 15:27:42 +0300

    kegliai - clean

--- index.php
+++ index.php
@@ -2,22 +2,22 @@
 
 $array = [
     'lane_1st' => [
-        'Pins_standing' => [
-            'Row_1' => 1,
-            'Row_2' => [0, 1],
-            'Row_3' => [0, 0, 1],
-            'Row_4' => [1, 0, 0, 1],
+        'pins_standing' => [
+            'row_1' => [1],
+            'row_2' => [0, 1],
+            'row_3' => [0, 0, 1],
+            'row_4' => [1, 0, 0, 1],
         ],
-        'Status' => 'ready',
+        'in_condition' => true,
     ],
     'lane_2st' => [
-        'Pins_standing' => [
-            'Row_1' => 0,
-            'Row_2' => [0, 0],
-            'Row_3' => [0, 0, 0],
-            'Row_4' => [0, 0, 0, 0],
+        'pins_standing' => [
+            'row_1' => [0],
+            'row_2' => [0, 0],
+            'row_3' => [0, 0, 0],
+            'row_4' => [0, 0, 0, 0],
         ],
-        'Status' => 'not_ready',
+        'in_condition' => false,
     ],
 ];
 var_dump($array);
