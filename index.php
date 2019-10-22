b5829a22f6a9fcf4005a219dff899a20eb33131a
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-22 15:55:21 +0300

    shots

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 $my_memories = [
     'baby driver',
     'baras',
@@ -11,19 +10,24 @@ $my_memories = [
 ];
 $h1 = 'Kas buvo penktadieni?!';
 $h2 = 'Ingos prisiminimai';
+
+$count = count($my_memories);
+$rand = rand(0, $count);
+$fb_text = $my_memories[$rand];
+$h3 = " Flashback $rand: $fb_text.";
 ?>
 <!doctype html>
 <html lang = "en">
-<head>
-<title> Penktadienio memories </title>
-</head>
-<body>
-   <h1> <?php print $h1; ?> </h1>
-   <h2> <?php print $h2; ?> </h2>
-   <ul><?php       foreach ($my_memories as $value) : ?>
-       <?php print $value; ?>
-       <br>
-       <?php endforeach; ?>
-   </ul>
-</body>
+    <head>
+        <title> Penktadienio memories </title>
+    </head>
+    <body>
+        <h1><?php print $h1; ?></h1>
+        <h2><?php print $h2; ?></h2>
+        <ul><?php foreach ($my_memories as $memory) : ?>
+                <li><?php print $memory; ?></li>
+            <?php endforeach; ?>
+        </ul>
+        <h3><?php print $h3; ?></h3>
+    </body>
 </html>
