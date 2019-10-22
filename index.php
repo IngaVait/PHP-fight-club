ec32118022c002bcf1b02f52b625be36e348a174
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-22 15:11:25 +0300

    mempries

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
 
 $my_memories = [
-    'taxi' => false,
-    'baby_driver' => true,
-    'baras' => true,
-    'gerimai' => [
-        'gin_tonic' => 5,
-        'water' => 'Fuck no!',
-        'shots' => 'Fuck yea!',
-    ],
-    'games' => true,
-    'awesome_memories' => true,
+    'baby_driver',
+    'baras',
+    'gin_tonic',
+    'no_water',
+    'shots',
+    'games',
+    'awesome_memories'
 ];
 var_dump($my_memories);
