a2d61e4b523c1b5cbe6becc7c719f46d826e137f
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-22 15:32:32 +0300

    baby driver

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
 
 $my_memories = [
-    'baby_driver',
+    'baby driver',
     'baras',
-    'gin_tonic',
-    'no_water',
+    'gin tonic',
+    'no water',
     'shots',
     'games',
-    'awesome_memories'
+    'awesome memories'
 ];
-var_dump($my_memories);
+$h1 = 'Kas buvo penktadieni?!';
+$h2 = 'Ingos prisiminimai';
+?>
+<!doctype html>
+<html lang = "en">
+<head>
+<title> Penktadienio memories </title>
+</head>
+<body>
+   <h1> <?php print $h1; ?> </h1>
+   <h2> <?php print $h2; ?> </h2>
+   <ul><?php       foreach ($my_memories as $value) : ?>
+       <?php print $value; ?>
+       <br>
+       <?php endforeach; ?>
+   </ul>
+</body>
+</html>
