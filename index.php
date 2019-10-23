5013fffe1bb3acc13c66c10a839de8bb4686f3da
IngaVait <55101873+IngaVait@users.noreply.github.com>
2019-10-23 14:24:30 +0300

    homework

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ $my_memories = [
     'no water',
     'shots',
     'games',
-    'awesome memories'
+    'awesome'
 ];
+
 $friend_memories = [
     'taxi',
     'baras',
@@ -17,16 +18,24 @@ $friend_memories = [
     'games',
     'no memories'
 ];
+
+foreach ($my_memories as $single_memory) {
+    if(in_array($single_memory, $friend_memories, true)){
+      $common_memories[]= $single_memory;
+    }
+}
+
 $rand = rand(0, count($my_memories) - 1);
 $fb_text = $my_memories[$rand];
 
 $h1 = 'Kas buvo penktadieni?!';
 $h2_1 = 'Ingos prisiminimai';
 $h2_2 = 'Draugo prisiminimai';
+$h2_3 = 'Vienodi prisiminimai';
 $h3 = "Flashback $rand: $fb_text.";
 ?>
 <!doctype html>
-<html lang = "en">
+<html lang="en">
     <head>
         <title> Penktadienio memories </title>
     </head>
@@ -40,6 +49,11 @@ $h3 = "Flashback $rand: $fb_text.";
         <h3><?php print $h3; ?></h3>
         <h2><?php print $h2_2; ?></h2>
         <ul><?php foreach ($friend_memories as $memory) : ?>
+                <li><?php print $memory; ?></li>
+            <?php endforeach; ?>
+        </ul>
+        <h2><?php print $h2_3; ?></h2>
+        <ul><?php foreach ($common_memories as $memory) : ?>
                 <li><?php print $memory; ?></li>
             <?php endforeach; ?>
         </ul>
